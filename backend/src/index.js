9accdb2fd5cc01bf634814d50dfdc7e8cefa8d7d
Abdul Rehman <abdulrahmankhan.8x@gmail.com>
2026-07-14 16:01:49 +0500

    URl Change

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173","http://192.168.18.7:5173"],
+    origin: ["http://localhost:5173","https://chatting-application-ivory.vercel.app"],
     credentials: true,
   }),
 );
