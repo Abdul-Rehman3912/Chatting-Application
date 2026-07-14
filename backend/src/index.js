6ba92ca673d5226e03accabe3e26ae6d5b050a2b
Abdul Rehman <abdulrahmankhan.8x@gmail.com>
2026-07-14 16:30:26 +0500

    handle error

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,15 +13,15 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
-const __dirname = path.resolve();
-
 app.use(
   cors({
     origin: ["http://localhost:5173","https://chatting-application-ivory.vercel.app"],
     credentials: true,
   }),
 );
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
--- frontend/src/Store/useAuth.Store.js
+++ frontend/src/Store/useAuth.Store.js
@@ -51,7 +51,7 @@ export const useAuthStore = create((set, get) => ({
 
       get().connectSocket();
     } catch (error) {
-      toast.error(error.response.data.message);
+      toast.error(error.response?.data?.message);
     } finally {
       set({ isLoggingIn: false });
     }
